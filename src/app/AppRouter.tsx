516b52031e0ed44fdf644edf6e11fa4355ff9e6d
Sergey Timofeev <drumliker@yandex.ru>
2026-02-15 00:09:29 +0300

    Protected Routes

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,13 +1,23 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Navigate,
+  Outlet,
+  Route,
+  Routes,
+} from "react-router-dom";
 import { Auth } from "@/pages/auth/ui/Auth";
 import { Product } from "@/pages/product/ui/Product";
 import { Products } from "@/pages/products/ui/Products";
-import { withAuth } from "@/shared/hoc/withAuth";
+import { isAuthenticated } from "@/shared/lib/auth";
 import { routes } from "@/shared/routes";
-import { withAnalytics } from "@/shared/hoc/withAnalytics";
 
-const ProtectedProducts = withAuth(Products);
-const ProtectedProduct = withAuth(withAnalytics(Product));
+function ProtectedRoute() {
+  if (!isAuthenticated()) {
+    return <Navigate to={routes.auth} replace />;
+  }
+
+  return <Outlet />;
+}
 
 export function AppRouter() {
   return (
@@ -17,9 +27,20 @@ export function AppRouter() {
           path={routes.root}
           element={<Navigate to={routes.products} replace />}
         />
-        <Route path={routes.auth} element={<Auth />} />
-        <Route path={routes.products} element={<ProtectedProducts />} />
-        <Route path={routes.product} element={<ProtectedProduct />} />
+        <Route
+          path={routes.auth}
+          element={
+            isAuthenticated() ? (
+              <Navigate to={routes.products} replace />
+            ) : (
+              <Auth />
+            )
+          }
+        />
+        <Route element={<ProtectedRoute />}>
+          <Route path={routes.products} element={<Products />} />
+          <Route path={routes.product} element={<Product />} />
+        </Route>
         <Route path={routes.notFound} element={<h1>404 Not Found</h1>} />
       </Routes>
     </BrowserRouter>
